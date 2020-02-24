45140a402896ea7d51aecd05d4f86860cf6b3fb3
muthuk <muthuk>
2020-02-24 06:31:33 +0530

    initial commit

---
+++ PopularArtifact/src/test/java/com/jfrog/util/PopularArtifact/PopularArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfrog.util.PopularArtifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopularArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
